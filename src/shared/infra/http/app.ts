1b7387ed113c068ecceac1b229eb564e9b745d84
Luis Paulo Pinto <lp_paulo85@yahoo.com.br>
2021-06-18 18:22:16 -0300

    Chore: Added CORS origin and credentials.

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -25,7 +25,12 @@ createConnection();
 const app = express();
 
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -36,6 +41,7 @@ app.use(
 );
 
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+
 app.use(routes);
 
 app.use(errorHandler);
